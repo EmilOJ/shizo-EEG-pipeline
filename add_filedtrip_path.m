d04dc48faa159ba134eb73b4ce0b49ecc65acc8d
Emil Johansen <emil.johansen@gmail.com>
2018-02-26 12:55:29 +0100

    Setup config and fieldtrip path

--- add_filedtrip_path.m
+++ add_filedtrip_path.m
@@ -1,4 +1,4 @@
 function [] = add_filedtrip_path()
-     [my_root, my_fieldtrip_path] = my_config();;
+     [my_root, my_fieldtrip_path] = my_config;
      addpath(my_fieldtrip_path);
 end
---
+++ run_pipeline.m
@@ -0,0 +1,6 @@
+clc; close all; clear all;
+
+add_filedtrip_path();
+
+
+%% Epoch data
