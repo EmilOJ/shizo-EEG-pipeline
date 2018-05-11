840cd33498e7b38dad475e49cccda8393674afa3
praktikanten <sigurd@laosdirg.com>
2018-05-11 12:08:28 +0200

    tilføjede ekstra output argument til config_sample

--- scripts_steadystate/config_sample.m
+++ scripts_steadystate/config_sample.m
@@ -1,4 +1,4 @@
-function [my_root, my_fieldtrip_path] = my_config()
+function [my_root, my_fieldtrip_path, my_output_folder] = my_config()
     %% Copy this config file, fill in the paths, and name it "my_config.m"
     my_root = ''; % Path to Roskilde folder
     my_output_folder = '';
