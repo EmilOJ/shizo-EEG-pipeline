700d39571d4a20178d3323561c0bad631a076ac1
Emil Johansen <emil.johansen@gmail.com>
2018-05-11 11:43:50 +0200

    update stuff

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-##Getting started
+## Getting started
 A folder `roskilde-eeg-pipeline/data` is required for the pipeline to run on data. This folder must only be saved locally due to the data access law. Ask Emil about this.
 The project is divided into two analysis: ERP (Event-related potentials, and steady-state. There is a corresponding folder for each analysis. The following instructions work for both.
 
 1. Create a copy of `config_sample.m` in the same directory and name it `my_config.m`.
-2. `my_config.m` contains absolute paths to 
+2. `my_config.m` contains absolute paths and could look like this:
+```
+my_root = '/Users/emiljohansen/Documents/Laosdirg/Roskilde'; % Path to root folder
+my_output_folder = '/Users/emiljohansen/Documents/Roskilde/steadystate'; % Path to processed data output
+my_fieldtrip_path = '/Users/emiljohansen/Documents/Laosdirg/Roskilde/roskilde-eeg-pipeline/fieldtrip'; % Path to fieldtrip
+```
+note that Fieldtrip is included in the repo
+
+3. Run the file `run_pipeline_steadystate.m`
--- scripts_steadystate/run_pipeline_steadystate.m
+++ scripts_steadystate/run_pipeline_steadystate.m
@@ -31,19 +31,20 @@ for i = 1:length(participants)
     end
 
     %% Read data
-%     initialize_participant_data(experiment, participant, ICA); 
+    initialize_participant_data(experiment, participant, ICA); 
 
     %% Filter
-%     my_filter(experiment, participant, ICA, true);
+    my_filter(experiment, participant, ICA, true);
 
     %% Epoch data
-%     epoch_data_1s(experiment, participant, ICA);
+    epoch_data_1s(experiment, participant, ICA);
 
-    %% Reject artifacts
+    %% Reject artifacts (manually) - only first time
+    %% Should be added to proc_notes.xlsx
 %     reject_artifacts_manual(experiment, participant, ICA);
 
     %%
-%     spectral_analysis(experiment, participant, ICA);
+    spectral_analysis(experiment, participant, ICA);
     
     %%
     close all;
@@ -51,26 +52,26 @@ for i = 1:length(participants)
 end
 
 %% Grand average
-    subjects = {...
-    '1001'
-    '1003'
-    '1004'
-    '1005'
-    '1006'
-    '1007'
-    '1008'
-    '1009'
-    '1011'
-    '1012'
-    '1014'    
-    };
-    % subjects = [14,13,5,16,15,17,21,12,8,25]% slowest
-    % subjects = [23,25]
-
-    grand_average(experiment, subjects);
-    % grand_average(experiment, subjects, 'stim');
-
-    GA_inspect(experiment, {'C21', 'A1', 'A23', 'D31'});
-    % GA_inspect(experiment, {'C21', 'A1', 'A23', 'D31'}, 'stim');
+%     subjects = {...
+%     '1001'
+%     '1003'
+%     '1004'
+%     '1005'
+%     '1006'
+%     '1007'
+%     '1008'
+%     '1009'
+%     '1011'
+%     '1012'
+%     '1014'    
+%     };
+%     % subjects = [14,13,5,16,15,17,21,12,8,25]% slowest
+%     % subjects = [23,25]
+% 
+%     grand_average(experiment, subjects);
+%     % grand_average(experiment, subjects, 'stim');
+% 
+%     GA_inspect(experiment, {'C21', 'A1', 'A23', 'D31'});
+%     % GA_inspect(experiment, {'C21', 'A1', 'A23', 'D31'}, 'stim');
 
     %% 
