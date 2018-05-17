ed8036c72cadc431302cd3af0577118f46150034
sigurd carlsen <sigurd@sigurds-MacBook-Pro.local>
2018-05-17 11:24:15 +0200

    added data_folder to output argument

--- config_sample.m
+++ config_sample.m
@@ -1,4 +1,4 @@
-function [my_root, my_output_folder] = my_config()
+function [my_root, my_output_folder, my_data_folder] = my_config()
     %% Copy this config file, fill in the paths, and name it "my_config.m"
     my_root = ''; % Path to Roskilde folder
     my_output_folder = ''; % Path to desired output folder
