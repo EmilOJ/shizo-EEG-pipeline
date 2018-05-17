b7892a435c24762203a66b8c08e5f5e70be50f61
sigurd carlsen <sigurd@sigurds-MacBook-Pro.local>
2018-05-17 16:34:27 +0200

    added input arguments to refchannel and channel; it seemd they were missing.

--- scripts/modules/rereference.m
+++ scripts/modules/rereference.m
@@ -2,10 +2,16 @@ function [data] = rereference(pars, data)
     cfg = [];
     cfg.reref = 'yes';
     cfg.refmethod = 'avg';
-    cfg.refchannel = get_channellist();
-    cfg.channel = get_channellist();
+    if ~isfield(pars,'experiment')
+        error('Missing parameter experiment')
+    end
+    if ~isfield(pars,'participant')
+        error('Missing parameter participant')
+    end
+    cfg.refchannel = get_channellist(pars.experiment,pars.participant);
+    cfg.channel = get_channellist(pars.experiment,pars.participant);
     
-    merge_pars_with_cfg(pars, cfg, 'rereference');
+    cfg = merge_pars_with_cfg(pars, cfg, 'rereference');
     
     data = ft_preprocessing(cfg, data);
 end
