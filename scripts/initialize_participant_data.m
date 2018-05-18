de2f73b6bd334cd94af447f27fe9650ae50f20e6
Emil Johansen <emil.johansen@gmail.com>
2018-05-18 09:49:55 +0200

    ENH: Generalize init data reading

--- scripts/initialize_participant_data.m
+++ scripts/initialize_participant_data.m
@@ -1,12 +1,14 @@
-function [] = initialize_participant_data(experiment, participant, ICA) 
-    %cfg = initialize_participant_cfg(experiment, participant, ICA);
-    [my_root, my_output_folder, my_data_folder]  = my_config()
-    cfg.dataset = [my_data_folder,filesep,'1001',filesep,'1001.e'];
+function [] = initialize_participant_data(participant) 
+    [my_root, my_output_folder, my_data_folder]  = my_config();
+    
+    cfg.dataset = [my_data_folder,filesep, participant ,filesep, participant '.e'];
     cfg.continuous = 'yes';
+    
+    % Read data
     data = ft_preprocessing(cfg);
     
     selectchannels = ft_channelselection({'all', '-25', '-EKG', '-Body Pos', '-CPAP', '-Pulse', '-Photic', '-Rate', '-IBI', '-Bursts', '-Suppr'}, data.label);
     data = ft_selectdata(data, 'channel', selectchannels);
     
-    save([my_data_folder,filesep,'1001',filesep,'data_init.mat'], 'data');
+    save([my_data_folder, filesep, participant ,filesep,'data_init.mat'], 'data');
 end
