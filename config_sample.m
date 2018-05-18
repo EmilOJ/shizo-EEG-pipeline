2e8db2773977ad85d8b06e9fa03a9f0f9a85f5d2
Emil Johansen <emil.johansen@gmail.com>
2018-05-18 09:49:27 +0200

    DOC: Update comment in config_sample

--- config_sample.m
+++ config_sample.m
@@ -1,6 +1,6 @@
 function [my_root, my_output_folder, my_data_folder] = my_config()
     %% Copy this config file, fill in the paths, and name it "my_config.m"
-    my_root = ''; % Path to Roskilde folder
+    my_root = ''; % Path to roskilde-eeg-pipeline folder
     my_output_folder = ''; % Path to desired output folder
     my_data_folder = ''; %path to data
 end
