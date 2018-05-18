8760531b63e3fc1a654e6280b4816d1aca6ea759
Emil Johansen <emil.johansen@gmail.com>
2018-05-18 09:32:15 +0200

    ENH: Refactor ICA_reconstruction for new pipeline architecture

--- scripts/ICA_reconstruction.m
+++ scripts/ICA_reconstruction.m
@@ -1,6 +1,8 @@
 function [data] = ICA_reconstruction(pars, data, ICA_components)
     cfg.channel = get_channellist();
     
+    cfg = merge_pars_with_cfg(pars, cfg, 'ICA_reconstruction');
+    
     %% Reconstruction from ICA components
     data = ft_rejectcomponent(cfg, ICA_components, data);
 end
