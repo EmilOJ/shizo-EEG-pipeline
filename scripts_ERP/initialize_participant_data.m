896b8fbedfb612bcb16b43e4441ae0d3e1fcc63f
Emil Johansen <emil.johansen@gmail.com>
2018-05-18 09:38:49 +0200

    ENH: Delete old data script, and move to scripts/

--- scripts_ERP/initialize_participant_data.m
+++
@@ -1,8 +0,0 @@
-function [] = initialize_participant_data(experiment, participant, ICA) 
-    cfg = initialize_participant_cfg(experiment, participant, ICA);
-    
-    cfg.dataset = cfg.files.raw;
-    cfg.outputfile = cfg.files.raw_mat;
-    
-    ft_preprocessing(cfg);
-end
